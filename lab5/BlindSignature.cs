2fc7faabdf49276162baff4a9593ccc1f6d8643d
Rubanov Vasiliy <ruban@DESKTOP-C4SU4T2>
2022-11-18 22:56:15 +0700

    fix lab5

--- lab5/BlindSignature.cs
+++ lab5/BlindSignature.cs
@@ -6,50 +6,140 @@ namespace lab5;
 
 public class BlindSignature
 {
-    public static void s()
+    public class Server
     {
-        BigInteger p, q, n, c, d, fi;
+        public BigInteger N { get; }
+        private readonly BigInteger _c;
+        public BigInteger D { get; }
 
-        do
+        public readonly List<BigInteger> Voters = new();
+
+        public int Yes;
+        public int No;
+
+
+        public Server()
         {
-            p = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(1024);
-        } while (!CryptoLib.PrimeExtensions.IsProbablyPrime(p));
+            BigInteger q;
+            BigInteger p;
+            do
+            {
+                p = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(1024);
+            } while (!CryptoLib.PrimeExtensions.IsProbablyPrime(p));
 
-        do
+            do
+            {
+                q = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(1024);
+            } while (!CryptoLib.PrimeExtensions.IsProbablyPrime(q) || p == q);
+
+            N = p * q;
+            var fi = (p - 1) * (q - 1);
+
+            List<BigInteger> gcd;
+            do
+            {
+                _c = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(256);
+                gcd = CryptoLib.Gcd(fi, _c);
+                D = gcd[2];
+            } while (gcd[0] != 1 || D < 0);
+        }
+
+        public BigInteger RegisterVoter(BigInteger h1)
         {
-            q = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(1024);
-        } while (!CryptoLib.PrimeExtensions.IsProbablyPrime(q) || p == q);
+            Voters.Add(h1);
+            return BigInteger.ModPow(h1, _c, N);
+        }
 
-        n = p * q;
-        fi = (p - 1) * (q - 1);
-
-        List<BigInteger> gcd;
-        do
+        public bool CheckBulletin(BigInteger n, BigInteger s)
         {
-            c = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(256);
-            gcd = CryptoLib.Gcd(fi, c);
-            d = gcd[2];
-        } while (gcd[0] != 1 || d < 0);
+            if (new BigInteger(SHA512.HashData(n.ToByteArray())) != BigInteger.ModPow(s, D, N))
+            {
+                return false;
+            }
 
+            if (Voters.Contains(s))
+            {
+                Console.WriteLine("Уже голосовал");
+                return false;
+            }
+
+            var bytes = n.ToByteArray();
+
+            var passport = BitConverter.ToInt32(bytes, 64);
+            var vote = bytes[^1];
+            Console.WriteLine("passport: {0} vote: {1}", passport, vote);
+
+            switch (vote)
+            {
+                case 1:
+                    No++;
+                    break;
+                case 2:
+                    Yes++;
+                    break;
+                default:
+                    Console.WriteLine("Error, incorrect vote");
+                    return false;
+            }
+
+            Voters.Add(s);
+            return true;
+        }
     }
 
-    public static BigInteger alice(BigInteger d, BigInteger N, int vote = 1)
+
+    public class Voter
     {
-        BigInteger r;
-        List<BigInteger> gcd;
-        do
+        public BigInteger H1 { get; }
+        private readonly BigInteger _n;
+        private readonly BigInteger r;
+        public BigInteger S { get; set; }
+        public BigInteger Message { get; set; }
+
+
+        public Voter(BigInteger N, BigInteger D, int passport, byte vote)
         {
-            r = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(256);
-            gcd = CryptoLib.Gcd(N, r);
-        } while (gcd[0] != 1);
+            _n = N;
+            var rnd = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(512);
+            var passBitArr = new BigInteger(passport).ToByteArray();
+            if (passBitArr.Length != 4)
+            {
+                passBitArr = passBitArr.Concat(new byte[] { 0 }).ToArray();
+            }
 
-        var rnd = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(512);
-        var n = BigInteger.Parse(rnd.ToString() + vote);
-        var h = SHA512.HashData(n.ToByteArray());
+            var voteBitArr = new BigInteger(vote).ToByteArray();
+            var temp3 = rnd.ToByteArray()
+                .Concat(passBitArr)
+                .Concat(voteBitArr)
+                .ToArray();
+            Message = new BigInteger(temp3);
 
-        var h1 = new BigInteger(h) * CryptoLib.ModPow(r, d, n) % n;
+            List<BigInteger> gcd;
+            do
+            {
+                r = CryptoLib.PrimeExtensions.RandomIntegerSizeBit(256);
+                gcd = CryptoLib.Gcd(N, r);
+            } while (gcd[0] != 1);
 
-        return h1;
+            var h = SHA512.HashData(Message.ToByteArray());
+            H1 = new BigInteger(h) * BigInteger.ModPow(r, D, N) % N;
+        }
+
+        public void CreateS(BigInteger s)
+        {
+            var rInv = ModInverse(r, _n);
+            S = s * rInv % _n;
+        }
     }
 
+    public static BigInteger ModInverse(BigInteger n, BigInteger p)
+    {
+        var inv = CryptoLib.Gcd(p, n)[2];
+        if (inv < 0)
+        {
+            inv += p;
+        }
+
+        return inv;
+    }
 }
--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using lab5;
 
-Console.WriteLine("Hello, World!");
+const int votersCount = 5;
+
+var server = new BlindSignature.Server();
+var voters = new List<BlindSignature.Voter>();
+
+var rnd = new Random();
+
+for (var i = 0; i < votersCount; i++)
+{
+    var voterData = new Tuple<int, byte>(rnd.Next(1000000, 9999999), (byte)rnd.Next(1, 3));
+    voters.Add(new BlindSignature.Voter(server.N, server.D, voterData.Item1,voterData.Item2));
+    Console.WriteLine("Passport: {0} vote: {1}",voterData.Item1,voterData.Item2);
+}
+
+Console.WriteLine();
+
+foreach (var voter in voters)
+{
+    if (!server.Voters.Contains(voter.H1))
+    {
+        var bulletin = server.RegisterVoter(voter.H1);
+        voter.CreateS(bulletin);
+        if (server.CheckBulletin(voter.Message, voter.S))
+        {
+            Console.WriteLine("Ok");
+        }
+    }
+}
+
+server.CheckBulletin(voters[3].Message, voters[3].S);
